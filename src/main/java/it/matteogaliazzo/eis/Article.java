2959b1b6792086b918349122068c0e037ebae137
Mattichs <mattiabastia@gmail.com>
2023-05-08 15:14:56 +0200

    read csv file and print lines

---
+++ src/main/java/it/matteogaliazzo/eis/Article.java
@@ -0,0 +1,38 @@
+package it.matteogaliazzo.eis;
+
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvCustomBindByPosition;
+/**
+ * classe rubata (preso ispirazione) da the guardian api
+ */
+
+//Identifier,URL,Title,Fulltext,Date,Source Set,Source
+public class Article {
+    @CsvBindByPosition(position = 0)
+    String id;
+    @CsvBindByPosition(position = 1)
+    String url;
+    @CsvBindByPosition(position = 2)
+    String title;
+    @CsvBindByPosition(position = 3)
+    String fullText;
+    @CsvBindByPosition(position = 4)
+    String date;
+    @CsvBindByPosition(position = 5)
+    String sourceSet;
+    @CsvBindByPosition(position = 6)
+    String source;
+    public Article(final String id, final String url, final String title, final String fullText, final String date, final String sourceSet, final String source) {
+        this.id = id;
+        this.url = url;
+        this.title = title;
+        this.fullText = fullText;
+        this.date = date;
+        this.sourceSet = sourceSet;
+        this.source = source;
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
+}
