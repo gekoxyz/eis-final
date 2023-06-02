de1e1a7143d3c1031d01e36e003257746e45f29f
dadebert <51121898+dadebert@users.noreply.github.com>
2023-06-02 17:43:35 +0200

    first main prototype

--- src/main/java/it/unipd/dei/eis/Main.java
+++ src/main/java/it/unipd/dei/eis/Main.java
@@ -4,37 +4,87 @@ import com.opencsv.exceptions.CsvValidationException;
 import it.unipd.dei.eis.adapters.NyTimesCsvAdapter;
 import it.unipd.dei.eis.adapters.TheGuardianJsonAdapter;
 import it.unipd.dei.eis.serialization.Serializer;
+import it.unipd.dei.eis.serialization.Deserializer;
+
 
 public class Main {
 
   public static void main(String[] args) {
     Serializer serializer = new Serializer();
+    Deserializer deserializer = new Deserializer();
 
-//    NyTimesCsvAdapter nyTimesCsvAdapter = new NyTimesCsvAdapter();
-//    nyTimesCsvAdapter.loadArticles();
-//    serializer.serialize(nyTimesCsvAdapter.getArticles(), "articles.xml");
+    /*
+     * -download
+     * java eis-final -d [sorgente] [query]
+     *
+     * -download e analisi
+     * java eis-final -da [sorgente] [query]
+     *
+     * -analisi
+     * java eis-final
+     * */
 
-//    NyTimesCsvAdapter nyTimesCsvAdapter2 = new NyTimesCsvAdapter("./assets/nytimes/temp.csv");
-//    try {
-//      nyTimesCsvAdapter2.loadArticles();
-//      serializer.serialize(nyTimesCsvAdapter2.getArticles());
-//    } catch (CsvValidationException e) {
-//      throw new RuntimeException(e);
-//    }
 
-//    Deserializer deserializer = new Deserializer();
+    if(args[0].equals("-d") || args[0].equals("-da"))
+      if(args[1].equals("nytimes"))
+      {
+        NyTimesCsvAdapter nyTimesCsvAdapter = new NyTimesCsvAdapter();
+        nyTimesCsvAdapter.loadArticles();
+        serializer.serialize(nyTimesCsvAdapter.getArticles(), "articles.xml");
 
-//    Article[] articles = deserializer.deserialize("articles.xml");
-//    System.out.println(articles.length + " articles in the XML file");
 
-//    TheGuardianJsonAdapter theGuardianJsonAdapter = new TheGuardianJsonAdapter("./assets/the_guardian/theguardian_articles_v1_p01.json");
-//    theGuardianJsonAdapter.loadArticles();
 
-//    serializer.serialize(theGuardianJsonAdapter.getArticles(), "articles.xml");
+        Article[] articles = deserializer.deserialize("articles.xml");
+        System.out.println(articles.length + " articles in the XML file");
+        if(args[0].equals("-da")) {
+          Analyzer.main(null);
+        }
+      }
+      else if(args[1].equals("theguardian"))
+      {
+        TheGuardianJsonAdapter theGuardianJsonAdapter1 = new TheGuardianJsonAdapter();
+        theGuardianJsonAdapter1.loadArticles();
+        serializer.serialize(theGuardianJsonAdapter1.getArticles(), "articles.xml");
 
-    TheGuardianJsonAdapter theGuardianJsonAdapter1 = new TheGuardianJsonAdapter();
-    theGuardianJsonAdapter1.loadArticles();
-    serializer.serialize(theGuardianJsonAdapter1.getArticles(), "articles.xml");
+        Article[] articles = deserializer.deserialize("articles.xml");
+        System.out.println(articles.length + " articles in the XML file");
+
+        if(args[0].equals("-da")) {
+          Analyzer.main(null);
+        }
+      }
+
+      else
+      {
+        Analyzer.main(null);
+      }
+
+
+
+
+
+
+
+
+
+
+
+      /*NyTimesCsvAdapter nyTimesCsvAdapter2 = new NyTimesCsvAdapter("./assets/nytimes/temp.csv");
+      try {
+        nyTimesCsvAdapter2.loadArticles();
+        serializer.serialize(nyTimesCsvAdapter2.getArticles(),"articles.xml");
+      } catch (CsvValidationException e) {
+          throw new RuntimeException(e);
+      }*/
+
+
+
+      /*TheGuardianJsonAdapter theGuardianJsonAdapter = new TheGuardianJsonAdapter("./assets/the_guardian/theguardian_articles_v1_p01.json");
+      theGuardianJsonAdapter.loadArticles();
+
+      serializer.serialize(theGuardianJsonAdapter.getArticles(), "articles.xml");*/
+
+
 
   }
 }
