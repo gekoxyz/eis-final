fe00f878faa14dc338e623c9ac0de8594f9ac597
Mattichs <mattiabastia@gmail.com>
2023-06-07 15:29:59 +0200

    updated tests

--- src/test/java/it/unipd/dei/eis/AnalyzerTest.java
+++ src/test/java/it/unipd/dei/eis/AnalyzerTest.java
@@ -1,4 +1,14 @@
 package it.unipd.dei.eis;
 
+import it.unipd.dei.eis.adapters.TheGuardianJsonAdapter;
+import org.junit.jupiter.api.Test;
+
 public class AnalyzerTest {
+
+    @Test
+    public void testApi() {
+        TheGuardianJsonAdapter adp = new TheGuardianJsonAdapter();
+
+        adp.callApi(10);
+    }
 }
