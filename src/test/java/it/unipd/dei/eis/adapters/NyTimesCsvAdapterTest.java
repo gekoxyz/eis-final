516b6100f7c53baf4926b91d580b2a196f420659
VenetoPin <matteorampin.mr@gmail.com>
2023-06-09 18:45:22 +0200

    changes

---
+++ src/test/java/it/unipd/dei/eis/adapters/NyTimesCsvAdapterTest.java
@@ -0,0 +1,9 @@
+package it.unipd.dei.eis.adapters;
+
+import junit.framework.TestCase;
+
+public class NyTimesCsvAdapterTest extends TestCase {
+
+  public void testLoadArticles() {
+  }
+}
---
+++ src/test/java/it/unipd/dei/eis/adapters/TheGuardianJsonAdapterTest.java
@@ -0,0 +1,15 @@
+package it.unipd.dei.eis.adapters;
+
+import junit.framework.TestCase;
+
+public class TheGuardianJsonAdapterTest extends TestCase {
+
+  public void testLoadArticles() {
+  }
+
+  public void testCallApi() {
+  }
+
+  public void testTestCallApi() {
+  }
+}
