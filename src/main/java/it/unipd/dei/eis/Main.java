ec5d53405b374bd6d479233e7ee4b82ec2c407b7
Matteo Galiazzo <mattegaliz@gmail.com>
2023-06-12 13:14:19 +0200

    added all missing javadocs

--- src/main/java/it/unipd/dei/eis/Main.java
+++ src/main/java/it/unipd/dei/eis/Main.java
@@ -1,106 +1,17 @@
 package it.unipd.dei.eis;
 
-import it.unipd.dei.eis.adapters.NyTimesCsvAdapter;
-import it.unipd.dei.eis.adapters.TheGuardianJsonAdapter;
-import it.unipd.dei.eis.serialization.Deserializer;
-import it.unipd.dei.eis.serialization.Serializer;
-
-import java.io.File;
-import java.io.IOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
- * Main for the Article Downloader
- */
-
-
-/**
- * Usage:
- * <p>
- * To Download articles use the following syntax:
- * java eis-final -d [source]</p>
- * <p>
- * To Download and Analyze articles use the following syntax:
- * java eis-final -da [source]
- * </p>
- * <p>
- * To Analyze articles use the following syntax:
- * java eis-final -a
- * </p>
- *
- * <p>Sources:
- * -nytimes
- * -theguardianapi
- * -theguardianlocal
- * </p>
- *
- *
- *
- * <p>
- * To add a source to the program:
- * -Create a custom adapter in the "src/main/java/it.unipd.dei.eis/adapters folder and extend the Adapter class"
- * -Add a case to the switch case in the main with the custom source name (e.g. case "new-source": [adapter-code];break;)
- * </p>
+ * The Main class serves as the entry point for the program.
+ * It initializes and runs the interactive menu.
  */
 public class Main {
 
+  /**
+   * The main method creates an instance of the InteractiveMenu class and calls its runMenu() method.
+   *
+   * @param args command line arguments (unused)
+   */
   public static void main(String[] args) {
-    InteractiveMenu interactiveMenu = new InteractiveMenu();
-    interactiveMenu.runMenu();
-//    File[] toSerialize = new File[]{new File("theguardian_articles_v1_p01.json"), new File("theguardian_articles_v1_p03.json"), new File("theguardian_articles_v1_p08.json"), new File("nytimes_articles_v2.csv")};
-//    interactiveMenu.chooseAdapterAndSerialize(toSerialize);
-//    Serializer serializer = new Serializer();
-//    serializer.serialize(new Article[]{new Article("title1", "text1"), new Article("title2", "text2")}, "articles.xml");
+    new InteractiveMenu().runMenu();
   }
-
-//  public static void main(String[] args) throws IOException {
-//    Serializer serializer = new Serializer();
-//    Deserializer deserializer = new Deserializer();
-//    Article[] articles;
-//
-//    if (args[0].equals("-d") || args[0].equals("-da")) {
-//      switch(args[1]){
-//        case "nytimes":
-//          NyTimesCsvAdapter nyTimesCsvAdapter = new NyTimesCsvAdapter();
-//          nyTimesCsvAdapter.loadArticles();
-//          serializer.serialize(nyTimesCsvAdapter.getArticles(), "articles.xml");
-//
-//          articles = deserializer.deserialize("articles.xml");
-//          System.out.println(articles.length + " articles in the XML file");
-//          if (args[0].equals("-da")) {
-//            Analyzer.main(null);
-//          }
-//          break;
-//
-//        case "theguardianlocal":
-//          TheGuardianJsonAdapter theGuardianJsonAdapter1 = new TheGuardianJsonAdapter();
-//          theGuardianJsonAdapter1.loadArticles();
-//          serializer.serialize(theGuardianJsonAdapter1.getArticles(), "articles.xml");
-//
-//          articles = deserializer.deserialize("articles.xml");
-//          System.out.println(articles.length + " articles in the XML file");
-//          if (args[0].equals("-da")) {
-//            Analyzer.main(null);
-//          }
-//          break;
-//        case "theguardianapi":
-//          TheGuardianJsonAdapter theGuardianJsonAdapter2 = new TheGuardianJsonAdapter();
-//          theGuardianJsonAdapter2.callApi();
-//          serializer.serialize(theGuardianJsonAdapter2.getArticles(), "articles.xml");
-//
-//          articles = deserializer.deserialize("articles.xml");
-//          System.out.println(articles.length + " articles in the XML file");
-//          if (args[0].equals("-da")) {
-//            Analyzer.main(null);
-//          }
-//          break;
-//      }
-//    }
-//    else
-//    {
-//      Analyzer.main(null);
-//    }
-//  }
 }
